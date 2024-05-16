9a6bed39a99c6678272b40a05ee658b6d4d88332
sri <srilakshmiT1207@gmail.com>
2024-05-16 22:15:41 +0530

    installing ansible

--- ansible/ansible.sh
+++ ansible/ansible.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 VAR=$(id -u)
-if ["$VAR" != 0]; then
-echo "please log in with root user"
-exit1
+if [ $VAR -ne 0 ]
+then
+    echo "please log in with root user"
 else
-echo "you are in root user"
+    echo "you are in root user"
 fi
 yum install ansible -y
