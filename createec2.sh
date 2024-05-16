f97c094d2b5fd31176ee2705e98f6b69975146b2
sri <srilakshmiT1207@gmail.com>
2024-05-16 17:17:15 +0530

    roboshop-shell

--- createec2.sh
+++ createec2.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 AMI="ami-0f3c7d07486cad139"
 Security_group="sg-02215d34f222346fe"
+instance_type1="t2.micro"
+type2="t3.micro"
+
 instance=("mongodb" "web" "catalouge")
 
 for i in "${instance[@]}"
 do
+if [ "$instance" -eq mongodb ] ||  [ "$instance" -eq web ]; then
+     INSTANCE_TYPE="t3.small"
+else
+     INSTANCE_TYPE="t2.small"
+fi
 
  aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids $Security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
     
