d8a66ca384ce9b81dde5a0d105768af84d3a3d96
sri <srilakshmiT1207@gmail.com>
2024-05-16 15:39:58 +0530

    roboshop-shell

--- createec2.sh
+++ createec2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 AMI="ami-0f3c7d07486cad139"
 Security_group="sg-02215d34f222346fe"
-INSTANCE_TYPE="t2.micro"
-instance=("Name=mongodb" "Name=web" "Name=catalouge")
+instance=("mongodb" "web" "catalouge")
 
 for i in "${instance[@]}"
 do
 
- INSTANCE_ID=$(aws ec2 run-instances --image-id "$AMI" --count 1 --instance-type "$INSTANCE_TYPE" --security-group-ids "$Security_group" --tag-specifications "ResourceType=instance,Tags=[$instance]" --query 'Instances[0].InstanceId' --output text)
-echo "lanching the instance $INSTANCE_ID"
+ aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids $Security_group --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    
+echo "lanching the instance $i"
 
 
 done
