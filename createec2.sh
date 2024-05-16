b80814ddd519249f22d48a34232972c8ba909a97
sri <srilakshmiT1207@gmail.com>
2024-05-16 14:27:13 +0530

    roboshop-shell

--- createec2.sh
+++ createec2.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 AMI="ami-0f3c7d07486cad139"
 Security_group="sg-02215d34f222346fe"
-
 instance=("mongodb" "web" "user")
+TAG_NAME="Name=$instance[@}]"
 for i in "${instance[@]}"
 do
 
-aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids $Security_group --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i},{Key=Environment,Value=roboshell}]'
+aws ec2 run-instances --image-id $AMI --instance-type t2.micro --security-group-ids $Security_group --tag-specifications 'ResourceType=instance,'
 echo "lanching the instance $i"
+aws ec2 create-tags --resources "$instance" --tags "$TAG_NAME" 
 
 done
